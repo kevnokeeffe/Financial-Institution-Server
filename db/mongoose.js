0bb9d8cdc48ff19a38209178bf8cf4efc1e7e254
Kevin O'Keeffe <kevokeeffe@gmail.com>
2020-02-23 20:37:20 +0000

    Mongo connection issue attempted fix 2

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 mongoose
     .connect(
-        `mongodb+srv://${process.env.MONGO_DB}:${process.env.MONGO_PASSWORD}@harmoney-server-q4jlk.mongodb.net/bankTest?retryWrites=true&w=majority`, { useNewUrlParser: true, useCreateIndex: true })
+        `mongodb+srv://${process.env.MONGO_DB}:${process.env.MONGO_PASSWORD}@harmoney-server-q4jlk.mongodb.net/bankTest?retryWrites=true&w=majority`, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
     .then(() => console.log('Successfully Connected to [ ' + db.name + ' ]'))
     .catch(err => console.log('Unable to Connect to [ ' + db.name + ' ]' + ' on all routes', err))
 let db = mongoose.connection
