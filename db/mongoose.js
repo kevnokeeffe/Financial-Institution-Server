c150e7f8248139bee513c42643a17987a3759d4a
Kevin O'Keeffe <kevokeeffe@gmail.com>
2020-02-23 15:38:17 +0000

    Add coverage report link to website

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 mongoose
     .connect(
-        process.env.MONGO_DB_WIT_BANK_STRING, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+        `mongodb+srv://${process.env.MONGO_DB}:${process.env.MONGO_PASSWORD}@harmoney-server-q4jlk.mongodb.net/bankTest?retryWrites=true&w=majority`, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
     .then(() => console.log('Successfully Connected to [ ' + db.name + ' ]'))
     .catch(err => console.log('Unable to Connect to [ ' + db.name + ' ]' + ' on all routes', err))
 let db = mongoose.connection
