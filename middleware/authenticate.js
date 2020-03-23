461d12ad1a9280b893a595c24cf20e817c9a20b5
Kevin O'Keeffe <kevokeeffe@gmail.com>
2020-03-23 22:49:27 +0000

    Update

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -54,11 +54,9 @@ router.verifyRToken = ((req, res, next) => {
 	const token = req.headers.authorization || req.headers['Authorization'];
 	if (!token)
 		return res.status(403).send({ auth: false, message: 'No token provided.' });
-
 	jwt.verify(token, process.env.SECRET_KEY_REFRESH, function(err, decoded) {
 		if (err)
 			return res.status(501).send({ auth: false, message: 'Failed to authenticate token.' });
-
 		// if everything good, save to request for use in other routes
 		req.userId = decoded.id;
 		next();
