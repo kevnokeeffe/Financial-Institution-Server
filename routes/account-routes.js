81de805e762220b3f9a48a800cc612771782e2b5
Kevin O'Keeffe <kevokeeffe@gmail.com>
2020-04-09 00:14:29 +0100

    Account update fix 1

--- routes/account-routes.js
+++ routes/account-routes.js
@@ -19,29 +19,15 @@ router.get('/find-savings-individual/:id', auth.verifyRToken, accountController.
 // Read one individual savings account by IBAN
 router.get('/find-savings-individual/iban/:iban', accountController.showIndividualSavingsAccountIBAN)
 
-// Update a spacific current account by id
-router.put('/update-current-account/:id', accountController.updateCurrentAccountID)
 
+// Current Account Balance Updating Routes
+router.post('/update-current-account-with/:id',auth.verifyRToken, accountController.updateTheCurrentAccount)
+router.post('/update-current-account-add/:iban',accountController.updateCurrentAccountAdd)
 
-// Current Account
-router.post('/update-current-account-with/:id', accountController.updateTheCurrentAccount)
-router.post('/update-current-account-add/:iban', accountController.updateCurrentAccountAdd)
+// Savings Account Balance Updating Routes
+router.post('/update-savings-account-with/:id',auth.verifyRToken, accountController.updateTheSavingsAccount)
+router.post('/update-savings-account-add/:iban',accountController.updateSavingsAccountAdd)
 
-// Savings Account
-router.post('/update-savings-account-with/:id', accountController.updateTheSavingsAccount)
-router.post('/update-savings-account-add/:iban', accountController.updateSavingsAccountAdd)
-
-
-
-
-
-// Update a current account by iban + funds
-router.put('/update-current-account/minus/:iban', accountController.updateCurrentAccountMinus)
-// Update a savings account by iban + funds
-router.put('/update-savings-account/minus/:iban', accountController.updateSavingsAccountMinus)
-
-// Update a specific savings account by id
-router.put('/update-savings-account/:id', accountController.updateSavingsAccountID)
 
 // Delete specific current account by id
 router.delete('/delete-current-account/:id', auth.verifyRToken, accountController.removeCurrentAccount)
